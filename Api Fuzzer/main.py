4426b2a100564c309e57e972476f7c12e830fc14
Hariom Singh <hariomsingh0398@gmail.com>
2023-11-25 20:31:50 +0530

    api fuzzer

---
+++ Api Fuzzer/main.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+
+
+def loop():
+    for word in sys.stdin:
+        res = requests.get(url=f"https://example.com/api/{word}")
+        if res.status_code == 404:
+            loop()
+        else:
+            print(res)
+            data = res.json() 
+            print(data)
